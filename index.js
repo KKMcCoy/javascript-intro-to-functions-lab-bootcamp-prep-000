47962be584c11c8bb90acc6a94cdae45d2549856
karen tsui <buzz.kyt@gmail.com>
2018-07-11 20:07:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function logShout(string) {
   console.log.toUpperCase()
 }
 
-function logWhisper(string) {
-  console.log("string.toLowerCase")
+function logWhisper() {
+  console.log.toLowerCase()
 }
 
 function sayHiToGrandma(string) {
