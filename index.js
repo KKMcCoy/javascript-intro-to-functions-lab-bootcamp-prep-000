f92406924b758fc9c7a07e8f1b1a60d8a72ef4ad
karen tsui <buzz.kyt@gmail.com>
2018-07-11 20:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
+  } else if (string === "I love u, Grandma.") {
+    return "I love you, too.";
   } else {
     return "YES INDEED!";
   }
 }
 
-function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can\'t hear you!";
-  } else {
-    return "YES INDEED!";
-  }
-}
